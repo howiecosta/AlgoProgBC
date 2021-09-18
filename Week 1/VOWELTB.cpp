b544e24567971b52f43cc855cbbc66368e58a92a
howie costa lim <howielip20@gmail.com>
2021-09-18 18:51:00 +0700

    Add 2 more files
    Add 2 more files to week 1 assignment folder

---
+++ Week 1/VOWELTB.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	
+	char c;
+	scanf("%c", &c);
+	
+	if 
+	(c=='A'||c=='I'|| c=='U'|| c=='E'|| c=='O')
+	printf("vowel");
+	
+	else 
+	printf("konsonan");
+
+	return 0;	
+}
---
+++ Week 1/start01.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	
+	int nomor;
+	scanf("%d" ,&nomor);
+	printf("%d" ,nomor);
+	return 0;
+}
